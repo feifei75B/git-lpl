a5187bc2b38535749f315fc2dab6558f11566cd8
git-office <1005738344@qq.com>
2019-07-24 14:10:24 +0800

    git-office commit

--- PythonFiles/QD_new/Device.py
+++ PythonFiles/QD_new/Device.py
@@ -1,7 +1,50 @@
+import os
+from Point import AgilentPoint
+
+
 class Device(object):
+    """器件类"""
 
     def __init__(self, paths: list, area_cm2, power_mw, wavelength_nm):
+        # 构造函数
         self.paths = paths
         self.area_cm2 = area_cm2
         self.power_mw = power_mw
         self.wavelength_nm = wavelength_nm
+        self.id = os.path.dirname(paths[0])
+        self.points = self.get_point_objects()
+
+    @staticmethod
+    def get_point_id(path):
+        # 获取id
+        basename = os.path.basename(path)
+        if basename[1].isdigit():
+            return int(basename[0:2])
+        else:
+            return int(basename[0])
+
+    def get_point_objects(self):
+        # 获取工作点对象，返回list
+        if '.txt' in self.paths[0]:
+            return []
+
+
+class SiliconDevice(Device):
+    """硅片器件类"""
+
+    def __init__(self, paths: list, area_cm2, power_mw, wavelength_nm):
+        # 构造函数，固定工作点面积
+        super().__init__(paths, area_cm2, power_mw, wavelength_nm)
+        self.areas_cm2 = [
+            0.0706858,
+            0.0025,
+            0.0001,
+            0.000025,
+            0.00000225,
+            0.00000225,
+            0.00000225,
+            0.00000225,
+            0.00000225,
+            0.0706858,
+            0.0706858
+        ]
--- PythonFiles/QD_new/test.py
+++ PythonFiles/QD_new/test.py
@@ -3,8 +3,8 @@ import pandas as pd
 path = r'E:\硕士\QDPD\data\IV测试\20190718 390\I273\1-iv-l.txt'
 df = pd.read_csv(path, sep='\t', header=0, names=['index', 'voltage', 'current'])
 df.drop(columns=['index'], inplace=True)
-dark = df.loc[0:200]
-light = df.loc[201:401]
-print(dark, type(light))
-dark.loc[0, 'current'] = 0
-print(df)
+dark = - df.loc[200:0:-1].reset_index(drop=True)
+light = - df.loc[401:201:-1].reset_index(drop=True)
+print((dark['voltage'] - -0.5).abs().min())
+print(dark.loc[(dark['voltage'] - -0.5).abs() == (dark['voltage'] - -0.5).abs().min(), 'current'].iloc[0])
+print(type(dark['voltage'] - -0.5))
